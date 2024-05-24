ffa6634b5edae81b305610f522a248028a8f867a
krishnavamsi-nk <krishnavamsin202@gmail.com>
2024-05-24 11:35:52 +0530

    updated server-server

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const { app } = require('./server.js')
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => {
+    console.log('server started on port', PORT)
+})
+
+//testing
---
+++ server/cacheClient/redis-client.js
@@ -0,0 +1,24 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_URI,
+        port: process.env.REDIS_PORT
+    }
+});
+
+(async () => {
+    await client.connect();
+})();
+client.on('connect', () => {
+    console.log('Redis client connected');
+});
+
+client.on('error', (err) => {
+    console.error('Redis connection error:', err);
+});
+
+module.exports = client;
+
+
